bee2c2f69ab44039bf58d4bc85c9550538a82743
ahmad <ahmadsaeed8081@gmail.com>
2023-12-04 06:15:04 +0500

    staking api

---
+++ src/app.js
@@ -0,0 +1,141 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require('cors');
+var bodyParser = require('body-parser'); 
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+})); 
+const DB="mongodb+srv://<username>:<password>@cluster0.xkfehzq.mongodb.net/?retryWrites=true&w=majority";
+// const DB1="mongodb://localhost:27017/myDB";
+
+// mongoose.connect("mongodb://localhost:27017/myDB")
+mongoose.connect(DB,{
+    useNewUrlParser :true,
+    useUnifiedTopology:true,
+})
+.then(()=>console.log("connecttion done"))
+.catch((err)=>console.log("no onnection"+err));
+
+const schema = new mongoose.Schema({
+    userAddress : String,
+    FName : String,
+    LName :String,
+    Email :String,
+    password :String,
+    Country :String,
+    Phone: String,
+    Ref_address:String,
+
+    date  :{
+        type: Date,
+        default : Date.now
+    }
+
+    
+
+});
+
+
+const collection = new mongoose.model("user",schema);
+
+
+
+const InsertData = async () =>{
+const data = new collection({
+    userAddress : "8988",
+    FName : "ahmad",
+    LName :"Saeed",
+    Email :"ahmadchohan007@gmail.com",
+    password :"ahmad@pass",
+    Country :"pakistan",
+    Phone: "03234354339",
+    Ref_address:"gkjbj96bmbuit7b"
+
+})
+  await data.save();
+}
+
+// const getData = async () =>{
+
+//     const result = await collection.find();
+//     console.log(result);
+// }
+
+
+app.get("/getdatabyaddress", async (req, res) => {
+    console.log("its working"+req.query.userAddress);
+
+    // res.json({ userAddress: req.query.userAddress });
+    const result = await collection.find({userAddress : req.query.userAddress});
+    // console.log(result);
+
+    res.send(result);
+
+})
+
+app.get("/getdatabymail", async (req, res) => {
+    console.log("its working"+req.query.userAddress);
+
+    // res.json({ userAddress: req.query.userAddress });
+    const result = await collection.find({userAddress : req.query.Email});
+    // console.log(result);
+
+    res.send(result);
+
+})
+
+
+
+app.post("/register", async (req, res) => {
+    console.log("its working");
+
+  
+        const data = new collection({
+            userAddress : req.body.userAddress,
+            FName : req.body.FName,
+            LName : req.body.LName,
+            Email : req.body.Email,
+            password : req.body.password,
+            Country : req.body.Country,
+            Phone : req.body.Phone,
+            Ref_address : req.body.Ref_address,
+
+
+        
+        })
+        const result =  await data.save();
+
+
+    res.send("User is Registerd");
+
+})
+
+app.get("/get", async (req, res) => {
+
+    const result = await collection.find({userAddress : req.query.userAddress});
+    res.send(result);
+
+})
+
+app.listen(port, () => {
+        console.log("connection is live"+ port);
+});
+
+// InsertData();
+// getData();
+
+
+
+
+
+
+
+
---
+++ src/temp.js
@@ -0,0 +1,128 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require('cors');
+var bodyParser = require('body-parser'); 
+
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+})); 
+const DB="mongodb+srv://chohanahmad1:yCZyxLivMcF6laEJ@cluster0.vvhbr9g.mongodb.net/?retryWrites=true&w=majority";
+// const DB1="mongodb://localhost:27017/myDB";
+
+// mongoose.connect("mongodb://localhost:27017/myDB");
+mongoose.connect(DB,{
+    useNewUrlParser :true,
+    useUnifiedTopology:true,
+})
+.then(()=>console.log("connecttion done"))
+.catch((err)=>console.log("no onnection"+err));
+
+const schema = new mongoose.Schema({
+    userAddress : String,
+    Name : String,
+    image :String,
+    date  :{
+        type: Date,
+        default : Date.now
+    }
+    
+
+});
+
+
+
+const collection = new mongoose.model("data",schema);
+
+
+
+const InsertData = async () =>{
+const data = new collection({
+    userAddress : "0900",
+    Name : "ahmad",
+    image:"bkjkbkjbjk",
+
+
+})
+  await data.save();
+}
+
+const getData = async () =>{
+    const result = await collection.find({_id : "6539cd40e118b75a2dcbf392"});
+    console.log(result);
+}
+
+app.get("/get", async (req, res) => {
+    console.log("its working"+req.query.userAddress);
+
+    // res.json({ userAddress: req.query.userAddress });
+    const result = await collection.find({userAddress : req.query.userAddress});
+    // console.log(result);
+
+    res.send(result);
+
+})
+
+app.patch("/user/:id",async (req, res)=>{
+
+    const _id=req.params.id;
+
+    const updtaedata= await collection.findByIdAndUpdate(_id,req.body);
+
+    res.send("its done");
+
+})
+
+app.post("/add", async (req, res) => {
+    console.log("its working");
+
+  
+        const data = new collection({
+            userAddress : req.body.userAddress,
+             Name : req.body.Name,
+             image : req.body.image,
+
+        
+        })
+        const result =  await data.save();
+
+
+    res.send("dfwsgs dcedsgvfd dfcbv df ");
+
+})
+
+app.get("/latest", async(req,res)=>{
+
+    const result = await collection.find().sort({$natural: -1 });
+    res.send(result);
+
+})
+
+
+app.get("/latestwinner", async(req,res)=>{
+
+    const result = await  collection.find().sort({$natural: -1 }).limit(1);
+    res.send(result);
+
+})
+
+app.listen(port, () => {
+        console.log("connection is live"+ port);
+});
+
+// InsertData();
+getData();
+
+
+
+
+
+
+
+
